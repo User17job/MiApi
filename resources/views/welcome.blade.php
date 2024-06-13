4db70401af5835d09d3b5f13658c4bbb68653ff2
Ramphys Tirana Brito <ramphystiranabrito@gmail.com>
2024-06-13 18:50:06 -0400

    UPDATETION

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -164,6 +164,8 @@
 
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
                         <p> &copy; by user17job </p>
+                                            <h2>Enjoy my students API</h2>
+                        <p> &copy; by user17job </p>
                     </footer>
                 </div>
             </div>
